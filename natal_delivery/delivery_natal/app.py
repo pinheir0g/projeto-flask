f10b3d6672b2310e6aee03455861f1b9505bc154
pinheir0g <gv.pinheiro@outlook.com>
2022-07-29 11:56:43 -0300

    ...

--- natal_delivery/delivery_natal/app.py
+++ natal_delivery/delivery_natal/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from delivery_ natal import views
+from delivery_natal import views
 
 def create_app():
     """Factory principal"""
 
     app = Flask(__name__)
     views.init_app(app)
-
+    
     return app
