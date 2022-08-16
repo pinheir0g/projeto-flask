2322eb0a531060162e9bdfc6a9e1d0f27850b342
pinheir0g <gv.pinheiro@outlook.com>
2022-08-16 17:51:20 -0300

    Att tests

--- natal_delivery/tests/test_app.py
+++ natal_delivery/tests/test_app.py
@@ -7,7 +7,7 @@ def test_app_is_created(app):
     assert app.name == 'natal_delivery.app'
 
 def test_config_is_loaded(config):
-    assert config['DEBUG'] is True
+    assert config['DEBUG'] is False
 
 def test_request_returns_200(client):
     assert client.get('/').status_code == 200
