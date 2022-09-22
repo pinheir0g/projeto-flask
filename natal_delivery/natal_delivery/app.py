158073a18a4eb4a56ebfcdacf82976cbb2d32a6b
pinheir0g <gv.pinheiro@outlook.com>
2022-09-22 15:26:36 -0300

    Add migrations and admin-interface

--- natal_delivery/natal_delivery/app.py
+++ natal_delivery/natal_delivery/app.py
@@ -4,7 +4,12 @@ from natal_delivery.ext import site
 from natal_delivery.ext import config
 from natal_delivery.ext import toolbar
 from natal_delivery.ext import db
+from natal_delivery.ext import migrate
 from natal_delivery.ext import cli
+from natal_delivery.ext import hooks
+from natal_delivery.ext import auth
+from natal_delivery.ext import admin
+
 
 # Application Factory
 
@@ -14,8 +19,12 @@ def create_app():
     app = Flask(__name__)
     config.init_app(app)
     db.init_app(app)
+    auth.init_app(app)
+    admin.init_app(app)
+    migrate.init_app(app)
     cli.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
+    hooks.init_app(app)
     return app
     
